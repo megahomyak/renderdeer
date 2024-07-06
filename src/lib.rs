72a8bfe1f2636204d3c0e7ee2caa3cd6f57cfe6f
megahomyak <g.megahomyak@gmail.com>
2024-07-06 21:34:10 +0500

    Fuck dem traits, I'm making a GAME

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 struct Pos {
     x: f64,
     y: f64,
@@ -15,32 +17,31 @@ struct Camera {
     direction: Pos,
 }
 
-struct Pixel {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8,
-}
+struct RGBA(u8, u8, u8, u8);
+struct RGB(u8, u8, u8);
 
 type UnitInterval = f64;
 
 trait Img {
-    fn get_pixel(&self, x: UnitInterval, y: UnitInterval) -> Option<Pixel>;
+    fn get_pixel(&self, x: UnitInterval, y: UnitInterval) -> RGBA;
 }
 
-trait Block<'a> {
-    type Img: Img + 'a;
-
-    fn btm<'a, I: Img + 'a>(&'a self) -> I;
-    fn top<'a, I: Img + 'a>(&'a self) -> I;
-    fn left<'a, I: Img + 'a>(&'a self) -> I;
-    fn right<'a, I: Img + 'a>(&'a self) -> I;
-    fn back<'a, I: Img + 'a>(&'a self) -> I;
-    fn front<'a, I: Img + 'a>(&'a self) -> I;
+trait Block {
+    fn btm(&self) -> Rc<dyn Img>;
+    fn top(&self) -> Rc<dyn Img>;
+    fn left(&self) -> Rc<dyn Img>;
+    fn right(&self) -> Rc<dyn Img>;
+    fn back(&self) -> Rc<dyn Img>;
+    fn front(&self) -> Rc<dyn Img>;
 }
 
-struct Grid<'a> {
-    blocks: Vec<Vec<Option<&'a dyn Block>>>,
+struct Grid {
+    blocks: Vec<Vec<Option<Rc<dyn Block>>>>,
 }
 
-fn render() {}
+fn render<const W: usize, const H: usize>(vert_fov: f64, hor_fov: f64) -> [[RGB; W]; H] {
+    use std::array::from_fn as array;
+    array(|height| array(|width| {
+        let pixel = RGB(0, 0, 0);
+    }))
+}
